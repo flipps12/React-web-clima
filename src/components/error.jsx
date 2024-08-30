54e4dbf30979030abaad7c734b96aec3af5721d6
Flipps12 <paginasypruebas12@gmail.com>
2024-08-30 17:40:27 -0300

    Error Message

--- src/components/error.jsx
+++
@@ -1,7 +0,0 @@
-export default function Error({ errorMessage }) {
-    return (
-        <div className="bg-gray-900 w-screen h-screen flex justify-center items-center">
-            <div className='text-white text-2xl'>{errorMessage}</div>
-        </div>
-    )
-}
