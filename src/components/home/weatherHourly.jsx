61d13d42c82c041c343f8545ccbe6c437bf06ed8
Flipps12 <paginasypruebas12@gmail.com>
2024-08-30 13:57:28 -0300

    clock AM/PM

--- src/components/home/weatherHourly.jsx
+++ src/components/home/weatherHourly.jsx
@@ -7,7 +7,7 @@ export default function WeatherHourly({ data, imageId }) {
     const clase = `size-16 bg-image-${imageId}`
     return (
         <section className="flex flex-col gap-3 items-center justify-center w-1/6 h-full">
-            <div className="text-slate-400 text-xl"> {date.getHours()}:00</div>
+            <div className="text-slate-400 text-base sm:text-lg"> {date.getHours() <= 13 ? date.getHours() + ':00 AM' : (date.getHours() - 12) + ':00 PM' }</div>
             <div id="image" className={clase}>{ }</div>
             {/* <div className="">{ data.weather[0].description }</div> */}
             <div className="text-3xl font-semibold">{ Math.round(data.temp) }°</div>
