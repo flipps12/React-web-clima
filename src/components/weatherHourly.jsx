69be44a5519f24b557868f083bd6bc32a8a24771
Flipps12 <paginasypruebas12@gmail.com>
2024-08-30 00:12:55 -0300

    home desktop adaptation

--- src/components/weatherHourly.jsx
+++ src/components/weatherHourly.jsx
@@ -4,15 +4,6 @@ export default function WeatherHourly({ data, imageId }) {
     const date = new Date(data.dt * 1000);
     var imageId;
 
-    // useEffect(() => {
-    //     const imageElement = document.getElementById('image');
-    //     if (data.weather[0].main === 'Clouds') {
-    //         imageElement.classList.toggle('bg-image-4');
-    //     } else if (data.weather[0].main === 'Clear') {
-    //         imageElement.classList.toggle('bg-image-1');
-    //     }
-    // }, [imageId]);
-
     const clase = `size-16 bg-image-${imageId}`
     return (
         <section className="flex flex-col gap-3 items-center justify-center w-1/6 h-full">
--- src/home.jsx
+++ src/home.jsx
@@ -38,34 +38,34 @@ export default function Home() {
     const date = new Date(JSON.stringify(weatherData.current.dt, null, 2) * 1000);
 
     return (
-        <div className='w-screen h-screen bg-gray-900 text-white'>
+        <div className='sm:flex w-[94vw] h-screen bg-gray-900 text-white  sm:mx-[3vw] sm:items-center sm:'>
             {/* <h3 className='inline-block'>Actual: </h3>
             <h3>{JSON.stringify(weatherData.timezone, null, 2)}</h3>
             <div className='inline-block'> {`${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`} </div> */}
 
-            <div className='flex flex-col sm:flex-row sm:justify-between sm:m-10'>
+            <div className='flex flex-col sm:flex-row sm:justify-between w-[94vw]'>
                 <div className=''>
                     <div className='flex h-32 mx-4 my-8'>
                         <div className='flex flex-col flex-1 justify-between h-full'>
                             <div>
-                                <h3 className='text-3xl'>Itu. EJ</h3>
+                                <h3 className='text-3xl sm:text-4xl'>Itu. EJ</h3>
                                 <p className='text-gray-500 text-lg'>Precipitaciones: {JSON.stringify(weatherData.hourly[0].pop, null, 2) * 100}%</p>
                             </div>
                             <h3 className='text-4xl font-bold'>{Math.round(JSON.stringify(weatherData.current.temp, null, 2))}°</h3>
                         </div>
                         <div className='flex flex-col justify-center items-end'>
-                            <div className={' size-28 bg-image-' + getIdImage(weatherData.current)}></div>
+                            <div className={' size-28 sm:size-36 bg-image-' + getIdImage(weatherData.current)}></div>
                             <p className='text-center text-gray-300 text-xl'>{JSON.stringify(weatherData.current.weather[0].description, null, 2)}</p>
                         </div>
                     </div>
-                    <article className='flex justify-center w-screen h-52 bg-gray-800 rounded-3xl sm:w-[56vw]'>
+                    <article className='flex justify-center w-screen h-52 bg-gray-800 rounded-3xl sm:w-[56vw] sm:h-44'>
                         <WeatherHourly data={weatherData.hourly[1]} imageId={getIdImage(weatherData.hourly[1])}></WeatherHourly>
                         <WeatherHourly data={weatherData.hourly[7]} imageId={getIdImage(weatherData.hourly[7])}></WeatherHourly>
                         <WeatherHourly data={weatherData.hourly[13]} imageId={getIdImage(weatherData.hourly[13])}></WeatherHourly>
                         <WeatherHourly data={weatherData.hourly[19]} imageId={getIdImage(weatherData.hourly[19])}></WeatherHourly>
                         <WeatherHourly data={weatherData.hourly[25]} imageId={getIdImage(weatherData.hourly[25])}></WeatherHourly>
                     </article>
-                    <article className='grid grid-cols-2 grid-rows-2 w-screen h-52 bg-gray-800 rounded-3xl mt-4 px-6 py-3  sm:w-[56vw]'>
+                    <article className='grid grid-cols-2 grid-rows-2 w-screen h-52 bg-gray-800 rounded-3xl mt-4 px-6 py-3  sm:w-[56vw]  sm:h-44'>
                         <WeatherDetails icon={"fa-wind"} weatherData={weatherData.current.wind_speed} name={"Viento:"} unit={"m/s"}></WeatherDetails>
                         <WeatherDetails icon={"fa-droplet"} weatherData={weatherData.current.humidity} name={"Húmedad:"} unit={"%"}></WeatherDetails>
                         <WeatherDetails icon={"fa-sun"} weatherData={weatherData.current.uvi} name={"UV indice:"}></WeatherDetails>
